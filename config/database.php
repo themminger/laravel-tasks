2189cd4a25bbbdb3fe84eab3485eed6860472cc0
themminger <152565398+themminger@users.noreply.github.com>
2025-02-13 17:48:33 -0700

    commas?>

--- config/database.php
+++ config/database.php
@@ -138,8 +138,8 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL')
-            'scheme' => 'tls'
+            'url' => env('REDIS_URL'),
+            'scheme' => 'tls',
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
